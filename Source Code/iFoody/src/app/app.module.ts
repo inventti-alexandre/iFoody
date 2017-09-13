aa4177fd4b44703c27ee0e9c8fc36f612ae4f798
Meo <1527032@hcmut.edu.vn>
2017-09-13 17:08:35 +0700

    create some component

--- Source Code/iFoody/src/app/app.module.ts
+++ Source Code/iFoody/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './global/bs-navbar/bs-navbar.component';
+import { FooterComponent } from './global/footer/footer.component';
+import { SearchBarComponent } from './global/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    FooterComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule
