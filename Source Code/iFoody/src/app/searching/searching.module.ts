604827c7b1fbbbee7d1d98bbdb796c0ffd882504
Tuan Pham <tuanbk08vn@gmail.com>
2017-10-03 15:55:58 +0700

    Done Search Filter, Google Map, Review Component (Searching Module)

--- Source Code/iFoody/src/app/searching/searching.module.ts
+++ Source Code/iFoody/src/app/searching/searching.module.ts
@@ -1,20 +1,38 @@
-import { MdCheckboxModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { RatingModule } from 'ngx-bootstrap/rating';
+import { SharedModule } from '../shared/shared.module';
+import { MdCheckboxModule, MatButtonModule } from '@angular/material';
 import { CollapseModule } from 'ngx-bootstrap';
 import { SearchFilterComponent } from './components/search-filter/search-filter.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MapComponent } from './components/map/map.component';
+import { AgmCoreModule } from '@agm/core';
+import { ReviewComponent } from './components/review/review.component';
+import { BsRatingComponent } from '../shared/components/bs-rating/bs-rating.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CollapseModule.forRoot(),
-    MdCheckboxModule
+    MdCheckboxModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCiZd64MtnFqNtuhvpWcGjBBjfzi0feu0Q'
+    }),
+    SharedModule,
+    RatingModule.forRoot(),
+    FormsModule,
+    MatButtonModule,
   ],
   exports: [
-    SearchFilterComponent
+    SearchFilterComponent,
+    MapComponent,
+    ReviewComponent
   ],
   declarations: [
-    SearchFilterComponent
+    SearchFilterComponent,
+    MapComponent,
+    ReviewComponent
   ]
 })
 export class SearchingModule { }
