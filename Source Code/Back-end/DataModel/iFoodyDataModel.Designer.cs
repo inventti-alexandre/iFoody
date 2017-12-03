2537147845a025815153980e1368837b9f537d74
Meo <1527032@hcmut.edu.vn>
2017-12-03 11:27:55 +0700

    Render new database

--- Source Code/Back-end/DataModel/iFoodyDataModel.Designer.cs
+++ Source Code/Back-end/DataModel/iFoodyDataModel.Designer.cs
@@ -1,5 +1,4 @@
-// T4 code generation is enabled for model 'D:\IT\DAI HOC\HK5\TTTN\iFoody\Source Code\Back-end\DataModel\iFoodyDataModel.edmx'. 
-// T4 code generation is enabled for model 'D:\DOCUMENT\HK6\TTTN\iFoody\Source Code\Back-end\DataModel\IFoodyDataModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\DOCUMENT\HK6\TTTN\iFoody\Source Code\Back-end\DataModel\iFoodyDataModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
