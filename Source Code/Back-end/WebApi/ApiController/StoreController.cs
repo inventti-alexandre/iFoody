b1f461324919f70c1c84aa131dc4799c4becdb74
Tuan Pham <tuanbk08vn@gmail.com>
2017-12-19 11:15:04 +0700

    Merge local and Remote Develop branch

---
+++ Source Code/Back-end/WebApi/ApiController/StoreController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.IServices;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebApi.ApiController
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/store")]
+    public class StoreController : System.Web.Http.ApiController
+    {
+        private readonly IStoreService _storeService;
+
+        public StoreController(IStoreService storeService)
+        {
+            _storeService = storeService;
+        }
+
+        // GET api/store
+        [HttpGet]
+        [Route("")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var stores = _storeService.GetAllStore();
+                if (stores != null)
+                {
+                    var storeEntities = stores.ToList();
+                    if (storeEntities.Any())
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, storeEntities);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotImplemented, "Got Exception");
+            }
+
+            return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Stores not found");
+        }
+
+    }
+}
